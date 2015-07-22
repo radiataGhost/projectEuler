886d3fb7cca86b1e8c048c0759b936a4f1594b37
radiataGhost <mabelloop@gmail.com>
2015-07-23 00:14:27 +1000

    problem2 added

--- problems.py
+++ problems.py
@@ -7,3 +7,17 @@ def problem_1:
 			result += x
 
 	return result
+
+def problem_2:
+	'''Return the sum of all even numbered fibonacci numbers under 4 million'''
+	fibonacci = 3
+	last = [1, 2]
+	result = 2
+
+	while last[1] <= 4000000:
+		if fibonacci % 2 == 0:
+			result += fibonacci
+		fibonacci = last[0] + last[1]
+		last = [last[1], fibonacci]
+
+	return result
